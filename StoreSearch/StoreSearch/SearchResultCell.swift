2da3e36b731d9893766f08ae59b0d608ce7918b1
Norman Lopez <noell.lpz@gmail.com>
2017-05-23 19:42:05 -0700

    Use custom cells for search results

---
+++ StoreSearch/StoreSearch/SearchResultCell.swift
@@ -0,0 +1,28 @@
+//
+//  SearchResultCell.swift
+//  StoreSearch
+//
+//  Created by Norman Lopez on 5/23/17.
+//  Copyright © 2017 Norman Lopez. All rights reserved.
+//
+
+import UIKit
+
+class SearchResultCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var artworkImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- StoreSearch/StoreSearch/SearchViewController.swift
+++ StoreSearch/StoreSearch/SearchViewController.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+struct TableViewCellIdentifiers {
+    static let searchResultCell = "SearchResultCell"
+    static let nothingFoundCell = "NothingFoundCell"
+}
+
 class SearchViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
@@ -22,6 +27,11 @@ class SearchViewController: UIViewController {
         super.viewDidLoad()
         
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right:0)
+        var cellNib = UINib(nibName: TableViewCellIdentifiers.searchResultCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.searchResultCell)
+        cellNib = UINib(nibName: TableViewCellIdentifiers.nothingFoundCell, bundle: nil)
+        tableView.register(cellNib, forCellReuseIdentifier: TableViewCellIdentifiers.nothingFoundCell)
+        tableView.rowHeight = 80
     }
     
     override func didReceiveMemoryWarning() {
@@ -67,23 +77,20 @@ extension SearchViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView,
                    cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cellIdentifier = "SearchResultCell"
-        var cell: UITableViewCell! = tableView.dequeueReusableCell(
-            withIdentifier: cellIdentifier)
         
-        if cell == nil {
-            cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellIdentifier)
-        }
         if searchResults.count == 0 {
-            cell.textLabel!.text = "(Nothing found)"
-            cell.detailTextLabel!.text = ""
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: TableViewCellIdentifiers.nothingFoundCell, for: indexPath)
+            return cell
         } else {
+            let cell = tableView.dequeueReusableCell(
+                withIdentifier: TableViewCellIdentifiers.searchResultCell, for: indexPath) as! SearchResultCell
             let searchResult = searchResults[indexPath.row]
-            cell.textLabel!.text = searchResult.name
-            cell.detailTextLabel!.text = searchResult.artistName
+            cell.nameLabel.text = searchResult.name
+            cell.artistNameLabel.text = searchResult.artistName
+            return cell
         }
         
-        return cell
     }
 }
 
@@ -103,5 +110,8 @@ extension SearchViewController: UITableViewDelegate {
             return indexPath
         }
     }
+    //    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat { //this doesn't work beacuase the height row is not loaded on ViewDidLoad
+    //        return 80
+    //    }
 }
 
